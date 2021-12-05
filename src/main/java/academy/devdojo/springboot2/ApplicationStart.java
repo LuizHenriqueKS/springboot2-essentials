de00dc1c4191ed9b03aca20014d7dbe2dbbe2fc3
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2021-12-05 10:29:31 -0300

    Spring Boot 2 Essentials 04 - @Component, @Autowired, @SpringBootApplication

--- src/main/java/academy/devdojo/springboot2/ApplicationStart.java
+++ src/main/java/academy/devdojo/springboot2/ApplicationStart.java
@@ -1,11 +1,16 @@
-package academy.devdojo.springboot2.start;
+package academy.devdojo.springboot2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-@ComponentScan(basePackages = "academy.devdojo.springboot2")
+@ComponentScan
+@Configuration
+// @ComponentScan(basePackages = "academy.devdojo.springboot2")
 public class ApplicationStart {
 
     public static void main(String[] args) {
